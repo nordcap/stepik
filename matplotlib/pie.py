06efff4d90b39660f9b5d9c6684f42ce557d64bf
Alex Budaev <budaevaa@mail.ru>
2017-03-03 11:28:55 +0700

    изменение кругового графика

--- matplotlib/pie.py
+++ matplotlib/pie.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 
 slices = [7, 2, 2, 13]
+
 activities = ['sleeping', 'eating', 'working', 'playing']
 colors = ['m', 'c', 'r', 'y']
+# Создать новое окно (фигуру) с одинаковыми размерами сторон (6 x 6 дюйма)
+#plt.figure(figsize=(7, 7))
+
+# Установим размеры осей по горизонтали и вертикали тоже одинаковыми
+#plt.axes([0.0, 0.0, 1.0, 1.0])
 
 plt.pie(slices, labels=activities, colors=colors, startangle=90, shadow=True, explode=(0, 0.1, 0, 0), autopct='%1.1f%%')
 plt.xlabel('X')
