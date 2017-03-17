3764a59b197bf1fe540d34ce3a79f228278444f3
Alex Budaev <budaevaa@mail.ru>
2017-03-17 09:27:01 +0700

    Project 'Alien Invasion': Назначение цвета фона

--- Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
@@ -7,6 +7,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Alien Invasion')
+    # Назначение цвета фона.
+    bg_color = (230, 230, 230)
 
     # Запуск основного цикла игры.
     while True:
@@ -14,6 +16,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+            # При каждом проходе цикла перерисовывается экран.
+            screen.fill(bg_color)
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
