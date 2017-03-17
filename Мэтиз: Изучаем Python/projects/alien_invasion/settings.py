c21db875188192bdc46df56663238289561628e3
Alex Budaev <budaevaa@mail.ru>
2017-03-17 14:08:00 +0700

    Project 'Alien Invasion': вынесение настроек в модуль settings

---
+++ Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
