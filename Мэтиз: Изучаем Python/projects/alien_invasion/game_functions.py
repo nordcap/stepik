376079a9caedf19de6731b1b536c7879d1131746
Alex Budaev <budaevaa@mail.ru>
2017-03-18 17:57:33 +0700

    Project 'Alien Invasion': обработка выстрелов

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -1,26 +1,32 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Обрабатывает нажатия клавиш и события мыши."""
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагирует на нажатие клавиш."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
-
+    elif event.key == pygame.K_SPACE:
+        # Создание новой пули и включение ее в группу bullets.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Реагирует на отпускание клавиш."""
@@ -30,10 +36,14 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Обновляет изображения на экране и отображает новый экран."""
     # При каждом проходе цикла перерисовывается экран.
     screen.fill(ai_settings.bg_color)
+    # Все пули выводятся позади изображений корабля и пришельцев.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()  # отрисовка корабля
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
