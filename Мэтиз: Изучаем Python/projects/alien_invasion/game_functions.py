25b20e86969d11096abba3afb9f45c74a3d3eb42
Alex Budaev <budaevaa@mail.ru>
2017-03-18 15:40:10 +0700

    Project 'Alien Invasion': рефакторинг

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -9,11 +9,25 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
+    """Реагирует на нажатие клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
