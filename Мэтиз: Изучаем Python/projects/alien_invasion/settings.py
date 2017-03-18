68bd2ea6686abd6705c5f183ae7a584703ed490c
Alex Budaev <budaevaa@mail.ru>
2017-03-18 17:42:02 +0700

    Project 'Alien Invasion': добавление класса Bullet (пуля)

--- Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
@@ -3,7 +3,12 @@ class Settings():
     def __init__(self):
         """Инициализирует настройки игры."""
         # Параметры экрана
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
