9c766912569e83011cf39e7e507720fb9ebb636e
Alex Budaev <budaevaa@mail.ru>
2017-03-18 17:46:50 +0700

    Project 'Alien Invasion': группировка пуль

--- Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -12,13 +13,16 @@ def run_game():
     pygame.display.set_caption('Alien Invasion')
     # Создание корабля.
     ship = Ship(screen, ai_settings)
+    # Создание группы для хранения пуль.
+    bullets = Group()
 
     # Запуск основного цикла игры.
     while True:
         # Отслеживание событий клавиатуры и мыши.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
