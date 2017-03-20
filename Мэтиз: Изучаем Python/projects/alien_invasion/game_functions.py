1e2915f2dd5b297628dd0d38d9a7b9f5f309b59e
Alex Budaev <budaevaa@mail.ru>
2017-03-20 14:01:31 +0700

    Project 'Alien Invasion': рефакторинг

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -74,15 +74,25 @@ def create_fleet(ai_settings, screen, aliens):
     """Создает флот пришельцев."""
     # Создание пришельца и вычисление количества пришельцев в ряду.
     # Интервал между соседними пришельцами равен одной ширине пришельца.
-
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
     # Создание первого ряда пришельцев.
     for alien_number in range(number_aliens_x):
         # Создание пришельца и размещение его в ряду.
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """Вычисляет количество пришельцев в ряду."""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """Создает пришельца и размещает его в ряду."""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
