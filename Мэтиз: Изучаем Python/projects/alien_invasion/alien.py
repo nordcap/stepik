a884c3d673fb1cf3f029ff9f164736fa914db30c
Alex Budaev <budaevaa@mail.ru>
2017-03-20 08:56:10 +0700

    Project 'Alien Invasion': класс Alien

---
+++ Мэтиз: Изучаем Python/projects/alien_invasion/alien.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Класс, представляющий одного пришельца."""
+
+    def __init__(self, ai_settings, screen):
+        """Инициализирует пришельца и задает его начальную позицию."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+        # Загрузка изображения пришельца и назначение атрибута rect.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Каждый новый пришелец появляется в левом верхнем углу экрана.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        # Сохранение точной позиции пришельца.
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Выводит пришельца в текущем положении."""
+        self.screen.blit(self.image, self.rect)
