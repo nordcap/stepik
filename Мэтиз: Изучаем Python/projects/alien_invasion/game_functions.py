428552eb0e7d8848a35409600a9f3d454faa1d2c
Alex Budaev <budaevaa@mail.ru>
2017-03-22 11:40:22 +0700

    Project 'Alien Invasion': запуск игры

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -5,7 +5,7 @@ from bullet import Bullet
 from alien import Alien
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Обрабатывает нажатия клавиш и события мыши."""
 
     for event in pygame.event.get():
@@ -17,6 +17,15 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Запускает новую игру при нажатии кнопки Play."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
