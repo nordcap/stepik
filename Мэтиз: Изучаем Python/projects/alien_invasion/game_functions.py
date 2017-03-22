6153814e1289f00d91df08e806ed2782d38c36bc
Alex Budaev <budaevaa@mail.ru>
2017-03-22 10:20:43 +0700

    Project 'Alien Invasion': флаг game_active , который завершает игру

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -162,13 +162,16 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Обрабатывает столкновение корабля с пришельцем."""
-    # Уменьшение ships_left.
-    stats.ships_left -= 1
-    # Очистка списков пришельцев и пуль.
-    aliens.empty()
-    bullets.empty()
-    # Создание нового флота и размещение корабля в центре.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-    # Пауза.
-    sleep(0.5)
+    if stats.ships_left > 0:
+        # Уменьшение ships_left.
+        stats.ships_left -= 1
+        # Очистка списков пришельцев и пуль.
+        aliens.empty()
+        bullets.empty()
+        # Создание нового флота и размещение корабля в центре.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        # Пауза.
+        sleep(0.5)
+    else:
+        stats.game_active = False
--- Мэтиз: Изучаем Python/projects/alien_invasion/game_stats.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_stats.py
@@ -5,6 +5,8 @@ class GameStats():
         """Инициализирует статистику."""
         self.ai_settings = ai_settings
         self.reset_stats()
+        # Игра Alien Invasion запускается в активном состоянии.
+        self.game_active = True
 
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
