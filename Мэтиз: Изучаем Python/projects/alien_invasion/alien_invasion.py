068ebdfa1199cc29f3e6008afe4b0e7d2ec706ba
Alex Budaev <budaevaa@mail.ru>
2017-03-22 10:23:41 +0700

    Project 'Alien Invasion': игра должна останавливаться после потери игроком последнего корабля.

--- Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/alien_invasion.py
@@ -29,9 +29,10 @@ def run_game():
     while True:
         # Отслеживание событий клавиатуры и мыши.
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
