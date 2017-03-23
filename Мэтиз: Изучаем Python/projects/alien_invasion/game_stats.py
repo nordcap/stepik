cb5c2642448cc62a5d61d7c121d7c92cc7368cfe
Alex Budaev <budaevaa@mail.ru>
2017-03-23 11:23:43 +0700

    Project 'Alien Invasion':сброс счетчика очков

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_stats.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_stats.py
@@ -11,3 +11,4 @@ class GameStats():
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
