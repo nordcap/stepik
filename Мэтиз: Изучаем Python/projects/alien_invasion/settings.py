4c84f39081c26585997f0b8e1b986ce1bbe12609
Alex Budaev <budaevaa@mail.ru>
2017-03-23 13:29:05 +0700

    Project 'Alien Invasion': Увеличение стоимости пришельцев

--- Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
@@ -18,6 +18,8 @@ class Settings():
         self.fleet_drop_speed = 10
         # Темп ускорения игры
         self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -35,3 +37,5 @@ class Settings():
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
