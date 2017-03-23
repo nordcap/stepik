747c352d792d66bec05a6295d0996613241af935
Alex Budaev <budaevaa@mail.ru>
2017-03-23 09:50:39 +0700

    Project 'Alien Invasion':изменение настроек скорости

--- Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/game_functions.py
@@ -26,6 +26,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Запускает новую игру при нажатии кнопки Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Сброс игровых настроек.
+        ai_settings.initialize_dynamic_settings()
+
         # Указатель мыши скрывается.
         pygame.mouse.set_visible(False)
 
@@ -98,8 +101,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     # При обнаружении попадания удалить пулю и пришельца.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
-        # Уничтожение существующих пуль и создание нового флота.
+        # Уничтожение пуль, повышение скорости и создание нового флота.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
+++ Мэтиз: Изучаем Python/projects/alien_invasion/settings.py
@@ -1,22 +1,35 @@
 class Settings():
     """Класс для хранения всех настроек игры Alien Invasion."""
+
     def __init__(self):
-        """Инициализирует настройки игры."""
+        """Инициализирует статические настройки игры."""
         # Параметры экрана
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         # Настройки корабля
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
         # Параметры пули
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3 #кол-во пуль
+        self.bullets_allowed = 3  # кол-во пуль
         # Настройки пришельцев
+        self.fleet_drop_speed = 10
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 1
         # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
