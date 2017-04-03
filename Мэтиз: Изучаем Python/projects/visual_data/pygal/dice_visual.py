906b24c5187aad3ab6dc5a98a25b6ef3e7712b1c
Alex Budaev <budaevaa@mail.ru>
2017-04-03 10:25:40 +0700

    Бросок двух кубиков

---
+++ Мэтиз: Изучаем Python/projects/visual_data/pygal/dice_visual.py
@@ -0,0 +1,28 @@
+import pygal
+from die import Die
+
+# Создание двух кубиков D6.
+die_1 = Die()
+die_2 = Die()
+
+# Моделирование серии бросков с сохранением результатов в списке.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Анализ результатов.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Визуализация результатов.
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 dice 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('dice_visual.svg')
