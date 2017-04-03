0138fc2ba98e233800fd940943a73c40a922f18e
Alex Budaev <budaevaa@mail.ru>
2017-04-03 13:54:33 +0700

    получение и вывод дат на графике

--- Мэтиз: Изучаем Python/projects/visual_data/load_data/highs_lows.py
+++ Мэтиз: Изучаем Python/projects/visual_data/load_data/highs_lows.py
@@ -1,13 +1,26 @@
 import csv
-# Чтение максимальных температур из файла.
+from matplotlib import pyplot as plt
+from datetime import datetime
 
+# Чтение дат и температурных максимумов из файла.
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
-    print(highs)
 
+# Нанесение данных на диаграмму.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+# Форматирование диаграммы.
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
