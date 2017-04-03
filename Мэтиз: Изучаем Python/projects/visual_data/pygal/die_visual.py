9ad3722326cb901f057c93da6f0be40ade7a32b8
Alex Budaev <budaevaa@mail.ru>
2017-04-03 10:19:10 +0700

    Построение гистограммы с помощью библиотеки pygal и сохранение результатов в файл svg

--- Мэтиз: Изучаем Python/projects/visual_data/pygal/die_visual.py
+++ Мэтиз: Изучаем Python/projects/visual_data/pygal/die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import pygal
 
 # Создание кубика D6.
 die = Die()
@@ -13,4 +14,13 @@ frequencies = []
 for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
-print(frequencies)
+
+# Визуализация результатов.
+hist = pygal.Bar()
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
