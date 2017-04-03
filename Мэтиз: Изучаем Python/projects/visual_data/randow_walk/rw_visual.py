6fa66397db92809b23eade0c576070bbe3b710a8
Alex Budaev <budaevaa@mail.ru>
2017-04-03 08:33:56 +0700

    Изменение размера диаграммы для заполнения экрана

--- Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
+++ Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
@@ -6,6 +6,10 @@ while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
     rw = RandomWalk(50000)
     rw.fill_walk()
+    # Назначение размера области просмотра.
+    plt.figure(figsize=(10, 6))
+
+
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
     # Выделение первой и последней точек.
