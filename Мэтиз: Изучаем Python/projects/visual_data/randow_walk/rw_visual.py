c7f5494cfec5c480134c7d1211afa46f9c035ecc
Alex Budaev <budaevaa@mail.ru>
2017-04-03 08:29:41 +0700

    Добавление точек

--- Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
+++ Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
@@ -4,10 +4,10 @@ from random_walk import RandomWalk
 # Новые блуждания строятся до тех пор, пока программа остается активной.
 while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
     # Выделение первой и последней точек.
     plt.scatter(0, 0, c='green', edgecolors='none', s=50)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
