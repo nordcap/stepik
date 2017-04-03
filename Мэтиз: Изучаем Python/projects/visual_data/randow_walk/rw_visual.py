bde0c15537f199c473c54c31cfa3513e0b746fbc
Alex Budaev <budaevaa@mail.ru>
2017-04-03 08:23:35 +0700

    назначение цветов градиента

--- Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
+++ Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
@@ -6,7 +6,8 @@ while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
