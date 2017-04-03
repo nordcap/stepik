6e1923a1095daae24b427281abedb2d59f7e70f2
Alex Budaev <budaevaa@mail.ru>
2017-04-03 08:26:25 +0700

    Начальные и конечные точки обозначаются зеленым и красным цветом

--- Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
+++ Мэтиз: Изучаем Python/projects/visual_data/randow_walk/rw_visual.py
@@ -8,6 +8,10 @@ while True:
     rw.fill_walk()
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    # Выделение первой и последней точек.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=50)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
