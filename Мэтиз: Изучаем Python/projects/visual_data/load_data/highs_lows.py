b6b4c3f34cced4fd99462f07a5f70e7f63992d0d
Alex Budaev <budaevaa@mail.ru>
2017-04-03 14:22:18 +0700

    Нанесение на диаграмму второй серии данных - минимумов температуры

--- Мэтиз: Изучаем Python/projects/visual_data/load_data/highs_lows.py
+++ Мэтиз: Изучаем Python/projects/visual_data/load_data/highs_lows.py
@@ -7,17 +7,19 @@ filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
+        low = int(row[3])
+        lows.append(low)
 
 # Нанесение данных на диаграмму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
-# Форматирование диаграммы.
+plt.plot(dates, lows, c='blue')
 # Форматирование диаграммы.
 plt.title("Daily high temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
