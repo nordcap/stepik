9ff12e5214afb8b772337000e00f0026f8518b5f
Alex Budaev <budaevaa@mail.ru>
2017-04-05 08:37:02 +0700

    Группировка стран по населению

--- Мэтиз: Изучаем Python/projects/visual_data/pygal/world/world_population.py
+++ Мэтиз: Изучаем Python/projects/visual_data/pygal/world/world_population.py
@@ -15,7 +15,20 @@ with open(filename) as f:
             code = get_country_code(country_name)
             if code:
                 cc_populations[code] = population
+                # Группировка стран по 3 уровням населения.
+                cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+                for cc, pop in cc_populations.items():
+                    if pop < 10000000:
+                        cc_pops_1[cc] = pop
+                    elif pop < 1000000000:
+                        cc_pops_2[cc] = pop
+                    else:
+                        cc_pops_3[cc] = pop
+    # Проверка количества стран на каждом уровне.
+    print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
     wm = pygal.maps.world.World()
     wm.title = 'World Population in 2010, by Country'
-    wm.add('2010', cc_populations)
+    wm.add('0-10m', cc_pops_1)
+    wm.add('10m-100m', cc_pops_2)
+    wm.add('>100m', cc_pops_3)
     wm.render_to_file('world_population.svg')
