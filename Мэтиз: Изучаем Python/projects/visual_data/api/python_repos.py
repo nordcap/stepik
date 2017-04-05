8a1870fdb05e13de5656f5624461a2c97b8fe21c
Alex Budaev <budaevaa@mail.ru>
2017-04-05 13:57:48 +0700

    Доработка диаграмм Pygal через конфигурацию

--- Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
+++ Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -10,12 +12,25 @@ print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
-
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+# Построение визуализации.
+# Построение визуализации.
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
