a9160d9d27a00853ff838a33032ddef2a95753aa
Alex Budaev <budaevaa@mail.ru>
2017-04-05 13:32:46 +0700

    Сводка самых популярных репозиториев

--- Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
+++ Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
@@ -1,4 +1,5 @@
 import requests
+
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
@@ -11,13 +12,10 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# Анализ первого репозитория.
-repo_dict = repo_dicts[0]
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
