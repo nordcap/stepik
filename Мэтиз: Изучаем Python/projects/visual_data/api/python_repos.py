81c35dad7c33782baf9b321ed357e594140272d7
Alex Budaev <budaevaa@mail.ru>
2017-04-05 15:19:19 +0700

    Нанесение данных на диаграмму

--- Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
+++ Мэтиз: Изучаем Python/projects/visual_data/api/python_repos.py
@@ -12,11 +12,15 @@ print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-# Построение визуализации.
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description'])
+    }
+    plot_dicts.append(plot_dict)
+
 # Построение визуализации.
 my_style = LS('#333366', base_style=LCS)
 my_config = pygal.Config()
@@ -32,5 +36,5 @@ chart = pygal.Bar(my_config, style=my_style)
 
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
