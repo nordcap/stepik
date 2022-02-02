0a7f15d8215130e2e11a8913a1dbf576df17e5b9
Alexey Budaev <budaevaa@mail.ru>
2022-02-02 21:15:16 +0700

    окончание решение задач по числовым типам данных

---
+++ Stepik-Поколение Python: курс для начинающих/6 Типы данных/6.1 Числовые типы данных /task_11.py
@@ -0,0 +1,7 @@
+'''
+Абсолютная сумма
+'''
+
+a1, a2, a3, a4, a5 = float(input()), float(input()), float(input()), float(input()), float(input())
+s = abs(a1) + abs(a2) + abs(a3) + abs(a4) + abs(a5)
+print(s)
