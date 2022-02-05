e36a74a36b94eeb6f124830c0270c252a718a797
Alexey Budaev <budaevaa@mail.ru>
2022-02-05 20:33:17 +0700

    Решение задач в модуле 6.3. Экзамен 3

---
+++ Stepik-Поколение Python: курс для начинающих/exam3/exam3_3.py
@@ -0,0 +1,10 @@
+'''
+Большое число
+'''
+from math import *
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+out = int(pow(a, b)) + int(pow(c, d))
+print(out)
+out = a**b + c**d
+print(out)
---
+++ Stepik-Поколение Python: курс для начинающих/exam3/exam3_4.py
@@ -0,0 +1,8 @@
+'''
+Размножение n-ок
+'''
+
+n = input()
+n2 = n * 2
+n3 = n * 3
+print(int(n) + int(n2) + int(n3))
