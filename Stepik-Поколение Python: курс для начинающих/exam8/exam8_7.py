0d132af86078334271500621f507c015d2074c86
Alexey Budaev <budaevaa@mail.ru>
2022-02-22 19:42:19 +0700

    Решение задач экзамена 8

--- Stepik-Поколение Python: курс для начинающих/exam8/exam8_7.py
+++ Stepik-Поколение Python: курс для начинающих/exam8/exam8_7.py
@@ -10,4 +10,4 @@ for a in range(1, 33):
         for c in range(1, 33):
             for d in range(1, 33):
                 if a ** 3 + b ** 3 == c ** 3 + d ** 3 and a != b and a != c and a != d and b != c and b != d and c != d:
-                    print(a ** 3 + b **
+                    print(a ** 3 + b ** 3)
