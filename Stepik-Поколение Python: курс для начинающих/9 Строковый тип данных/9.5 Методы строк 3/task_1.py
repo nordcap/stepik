b79c4f3e07e034897bbe4948e2b22860d79844bc
Alexey Budaev <budaevaa@mail.ru>
2022-02-27 14:03:29 +0700

    Решение задач по модулю 9.5 Методы строк 3

---
+++ Stepik-Поколение Python: курс для начинающих/9 Строковый тип данных/9.5 Методы строк 3/task_1.py
@@ -0,0 +1,9 @@
+'''
+Дополните приведенный код, используя форматирование строк с помощью метода format, так чтобы он вывел текст:
+
+«In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
+'''
+
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(2010, '10k', 'Bitcoin')
+
+print(s)
---
+++ Stepik-Поколение Python: курс для начинающих/9 Строковый тип данных/9.5 Методы строк 3/task_2.py
@@ -0,0 +1,9 @@
+'''
+Дополните приведенный код, используя форматирование строк с помощью f-строк, так чтобы он вывел текст:
+«In 2010, someone paid 10K Bitcoin for two pizzas.» (без кавычек).
+'''
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
