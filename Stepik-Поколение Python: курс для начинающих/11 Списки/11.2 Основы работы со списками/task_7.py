29fd8389e7b059ef210425bec1d45f1b2afb4e7d
Alexey Budaev <budaevaa@mail.ru>
2022-03-01 22:18:02 +0700

    Решение задач по модулю 11.2 Основы работы со списками

---
+++ Stepik-Поколение Python: курс для начинающих/11 Списки/11.2 Основы работы со списками/task_7.py
@@ -0,0 +1,9 @@
+'''
+Дополните приведенный код так, чтобы он вывел элементы списка languages в обратном порядке.
+Примечание. Для вывода списка воспользуйтесь функцией print().
+'''
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese',
+             'Lahnda']
+
+print(languages[::-1])
