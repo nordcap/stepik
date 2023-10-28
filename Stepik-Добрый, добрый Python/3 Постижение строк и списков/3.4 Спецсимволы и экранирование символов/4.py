28f717b85177462b5b674fd108a9bee91a41afbe
Alexey Budaev <budaevaa@mail.ru>
2023-10-28 22:10:29 +0700

    Раздел 3.4

---
+++ Stepik-Добрый, добрый Python/3 Постижение строк и списков/3.4 Спецсимволы и экранирование символов/4.py
@@ -0,0 +1,5 @@
+'''
+Подвиг 5. Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts. Результат отобразите на экране
+'''
+
+print(r'C:\WINDOWS\System32\drivers\etc\hosts')
